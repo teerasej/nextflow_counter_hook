1f0801b9190f0d9b083e8f81048ee81ad2482eb8
Teerasej <teerasej@gmail.com>
2020-07-21 17:43:12 +0700

    first commit and applied react hook

---
+++ android/app/src/main/kotlin/com/example/nextflow_counter_hook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nextflow_counter_hook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nextflow Flutter Widget Today',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    var _counter = useState(0);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Nextflow React Hook 1'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '${_counter.value}',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _counter.value = _counter.value + 1;
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
